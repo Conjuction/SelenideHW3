79884a72951bd94fcbf1e1c21b8ef2a9e6bcfd2f
Conjuction <suhinin-d@mail.ru>
2023-01-11 15:04:26 +0500

    add test

---
+++ src/test/java/Conflict.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Conflict {
+    @Test
+    void solutionsHover(){
+        open("https://github.com/");
+        $("div.header-menu-wrapper").$(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $("h1.h1-mktg.mb-3").shouldHave(text("Build like the best"));
+    }
+}
